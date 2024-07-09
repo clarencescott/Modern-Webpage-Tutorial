5e16eb9c5a86ab275cd1b347776312e214297b0b
Clarence Scott <111468756+clarencescott@users.noreply.github.com>
2024-07-09 11:22:09 -0400

    Modern WebPage Tutorial
    Modern WebPage Tutorial - Website Files

---
+++ script.js
@@ -0,0 +1,9 @@
+//This is a simple funciton to create an alert bar to show JavaScript is working
+
+//Searching the document for the id 'jsButton'
+//Created an click event listener to notice when 'jsButton' is clicked
+//Created a functino to set an alert menu to inform it works
+//Enjoy the fruits of my labor
+document.getElementById('jsButton').addEventListener('click', function(){
+    alert('Congratulations! Your JavaScript Works!!!');
+});
